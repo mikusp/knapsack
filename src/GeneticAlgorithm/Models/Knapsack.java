96a97c02010766dd519ad37fe3a246077bbc423c
Marcin Mikołajczyk <mikus12@gmail.com>
2013-03-11 23:37:03 +0100

    Knapsack and Population initial implementation

--- src/GeneticAlgorithm/Models/Knapsack.java
+++ src/GeneticAlgorithm/Models/Knapsack.java
@@ -1,4 +1,50 @@
 package GeneticAlgorithm.Models;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Knapsack is an equivalent of genome in our algorithm.
+ * It consists of zero or more items.
+ */
 public class Knapsack {
+
+    private final ArrayList<Item> items = new ArrayList<>();
+    private int sizeConstraint;
+
+    /**
+     * Creates new Knapsack with random items inside.
+     * Probability of including an item is 50%.
+     * @param itemsAvailable collection of available items
+     */
+    public Knapsack(int sizeConstraint, ItemCollection itemsAvailable) {
+        this.sizeConstraint = sizeConstraint;
+
+        Random r = new Random();
+        for (Item i : itemsAvailable.getAllItems()) {
+            if (r.nextBoolean())
+                items.add(i);
+        }
+    }
+
+    /**
+     * Computes fitness of a knapsack.
+     * Value of an item is added to overall fitness only if
+     * the item 'fits' in the knapsack, that is (size of previous
+     * items + size of the item) <= size limit.
+     * @return computed fitness
+     */
+    public int fitness() {
+        int fitness = 0;
+        int currentSize = 0;
+
+        for (Item i : items) {
+            currentSize += i.getSize();
+
+            if (currentSize <= sizeConstraint)
+                fitness += i.getValue();
+        }
+
+        return fitness;
+    }
 }
--- src/GeneticAlgorithm/Models/Population.java
+++ src/GeneticAlgorithm/Models/Population.java
@@ -1,4 +1,28 @@
 package GeneticAlgorithm.Models;
 
+import java.util.ArrayList;
+
 public class Population {
+
+    private int populationSize;
+    private int knapsackSize;
+    private ArrayList<Knapsack> knapsacks;
+
+    public Population(int populationSize, int knapsackSize, ItemCollection items) {
+        this.populationSize = populationSize;
+        this.knapsackSize = knapsackSize;
+        this.createPopulation(populationSize, knapsackSize, items);
+    }
+
+    /**
+     * Creates random population of knapsacks
+     * @param populationSize
+     * @param knapsackSize size limit of each knapsack
+     * @param items all available items
+     */
+    private void createPopulation(int populationSize, int knapsackSize, ItemCollection items) {
+        for (int i = 0; i < populationSize; ++i) {
+            knapsacks.add(new Knapsack(knapsackSize, items));
+        }
+    }
 }
