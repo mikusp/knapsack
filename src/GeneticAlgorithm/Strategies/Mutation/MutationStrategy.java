e8b6a26c4c33f3d673710a12f56bbb2c0313c2f4
rampler <rampler@gmail.com>
2013-03-15 22:06:06 +0100

    Question in MutationStrategy
    Question about mutate() parameters.

--- src/GeneticAlgorithm/Strategies/Mutation/MutationStrategy.java
+++ src/GeneticAlgorithm/Strategies/Mutation/MutationStrategy.java
@@ -16,5 +16,8 @@ public abstract class MutationStrategy {
      * @param itemsAvailable
      *
      */
-    public abstract void mutate(Knapsack knapsack, ItemCollection itemsAvailable);
+    public abstract void mutate(Knapsack knapsack, ItemCollection itemsAvailable); //Czemu mamy tutaj drugi parametr itemCollection?
+    //Nie możemy po prostu dopisać do Kanpsack fukcji getItemsAvaible() i przekawywać jeden parametr?
+    //btw. wybacz, że tak, ale nie oganiam githuba i nie wiem gdzie sa komentarze
+    //Mateusz
 }
