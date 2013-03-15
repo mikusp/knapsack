8b169177404f0f52728506cddeb894da61c61a47
rampler <rampler@gmail.com>
2013-03-15 22:20:00 +0100

    Update CrossoverStrategy.java

--- src/GeneticAlgorithm/Strategies/Crossover/CrossoverStrategy.java
+++ src/GeneticAlgorithm/Strategies/Crossover/CrossoverStrategy.java
@@ -1,4 +1,13 @@
 package GeneticAlgorithm.Strategies.Crossover;
 
+import GeneticAlgorithm.Models.Knapsack;
+
 public abstract class CrossoverStrategy {
+
+    /**
+     * Krzyżowanie genów dwóch plecaków w reprodukcji.
+     * Istnieje prawdopodobieństwo stworzenia słabszego plecaka.
+     * @param knapsack
+     */
+    public abstract void crossover(Knapsack knapsack);
 }
