3bb0f9dacbe05cb9156cd9104f8afedb42b19b33
rampler <rampler@gmail.com>
2013-03-15 22:20:46 +0100

    Create SplitStrategy.java

---
+++ src/GeneticAlgorithm/Strategies/Crossover/SplitStrategy.java
@@ -0,0 +1,16 @@
+package GeneticAlgorithm.Strategies.Crossover;
+
+import GeneticAlgorithm.Models.Knapsack;
+
+public class SplitStrategy {
+
+    /**
+     * Metoda podziału genów na lewą i prawą stronę,
+     * Bierzemy losową ilość genów jednego plecaka i krzyżujemy z pozostałymi genami drugiego plecaka
+     * @param knapsack
+     */
+    public void crossover(Knapsack knapsack)
+    {
+        //TODO - Pomyślę nad tym jutro :)
+    }
+}
