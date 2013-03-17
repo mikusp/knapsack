27a8bba10d4dfe60bc27a1cdfbbd86e9d4bd1a3c
Marcin Mikołajczyk <mikus12@gmail.com>
2013-03-17 22:40:18 +0100

    Stub Algorithm class

--- src/GeneticAlgorithm/Algorithm/Algorithm.java
+++ src/GeneticAlgorithm/Algorithm/Algorithm.java
@@ -1,4 +1,37 @@
 package GeneticAlgorithm.Algorithm;
 
+import GeneticAlgorithm.Models.Population;
+import GeneticAlgorithm.Strategies.Crossover.CrossoverStrategy;
+import GeneticAlgorithm.Strategies.Mutation.MutationStrategy;
+import GeneticAlgorithm.Strategies.Selection.SelectionStrategy;
+
 public class Algorithm {
+    // TODO add state
+
+    private final SelectionStrategy selectionStrategy;
+    private final CrossoverStrategy crossoverStrategy;
+    private final MutationStrategy mutationStrategy;
+    private Population currentPopulation;
+
+    public Algorithm(CrossoverStrategy crossoverStrategy,
+                     SelectionStrategy selectionStrategy,
+                     MutationStrategy mutationStrategy,
+                     Population initialPopulation) {
+        this.crossoverStrategy = crossoverStrategy;
+        this.selectionStrategy = selectionStrategy;
+        this.mutationStrategy = mutationStrategy;
+        this.currentPopulation = initialPopulation;
+    }
+
+    /**
+     * Runs a single step of an algorithm.
+     * It should move currentPopulation to oldPopulation,
+     * create newPopulation, run (select, crossover, mutate) in a loop
+     * until newPopulation is the same size as an oldPopulation.
+     * State should be changed to Stepping.
+     * TODO
+     */
+    public void step() {
+
+    }
 }
