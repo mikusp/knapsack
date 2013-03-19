a62e137ece3e46557cecb3a19d3edac128247c04
Marcin Mikołajczyk <mikus12@gmail.com>
2013-03-19 15:27:01 +0100

    Initial algorithm implementation

--- src/GeneticAlgorithm/Algorithm/Algorithm.java
+++ src/GeneticAlgorithm/Algorithm/Algorithm.java
@@ -1,10 +1,14 @@
 package GeneticAlgorithm.Algorithm;
 
+import GeneticAlgorithm.Models.Knapsack;
 import GeneticAlgorithm.Models.Population;
 import GeneticAlgorithm.Strategies.Crossover.CrossoverStrategy;
 import GeneticAlgorithm.Strategies.Elitism.ElitismStrategy;
 import GeneticAlgorithm.Strategies.Mutation.MutationStrategy;
 import GeneticAlgorithm.Strategies.Selection.SelectionStrategy;
+
+import java.util.Collection;
+import java.util.List;
 
 public class Algorithm {
     // TODO add state
@@ -15,6 +19,14 @@ public class Algorithm {
     private final ElitismStrategy elitismStrategy;
     private Population currentPopulation;
 
+    /**
+     * Maybe Factory will be better with this many parameters?
+     * @param crossoverStrategy
+     * @param selectionStrategy
+     * @param mutationStrategy
+     * @param elitismStrategy
+     * @param initialPopulation
+     */
     public Algorithm(CrossoverStrategy crossoverStrategy,
                      SelectionStrategy selectionStrategy,
                      MutationStrategy mutationStrategy,
@@ -52,6 +64,33 @@ public class Algorithm {
      * TODO
      */
     public void step() {
+        // current WIP
 
+        // Step 1. Create empty population
+        Population newPopulation = new Population(currentPopulation.getKnapsackSize(),
+                currentPopulation.getItemCollection());
+
+        // Step 2. Copy elite to new population
+        elitismStrategy.copyElite(currentPopulation, newPopulation);
+
+        // Step 3. Breed genomes until new population is as big as old population
+        // Apparent problem: if currentPopulation size is odd, newPopulation
+        // will be bigger. Similarily, if it's even and we copy elite that's size
+        // is odd we have the same situation.
+        // definitely TODO
+        while (newPopulation.getPopulationSize() < currentPopulation.getPopulationSize()) {
+            List<Knapsack> parents = selectionStrategy.select(currentPopulation);
+
+            Collection<Knapsack> offspring =   // getting indices is very ugly
+                    crossoverStrategy.crossover(parents.get(0), parents.get(1));
+
+            for (Knapsack k : offspring)
+                mutationStrategy.mutate(k);
+
+            newPopulation.add(offspring);
+
+            newPopulation.updateSize();
+            currentPopulation = newPopulation;
+        }
     }
 }
