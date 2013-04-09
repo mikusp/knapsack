e4180f5f1645ccd240cb624d4f3a4ff897c32e6c
rampler <rampler@gmail.com>
2013-04-10 00:37:16 +0300

    Creating ListManager.java -Saving and Loading List

---
+++ src/GUI/ListManager.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+
+public class ListManager {
+  
+	public void saveList(File file, TableModel model) throws FileNotFoundException
+	{
+		PrintWriter out = new PrintWriter(file);
+		for(int i=0; i<model.getRowCount(); i++)
+		{
+			out.write((String)model.getValueAt(i, 0)+"\n");
+			out.write(model.getValueAt(i, 1)+"\n");
+			out.write(model.getValueAt(i, 2)+"\n");
+		}
+		out.close();
+	}
+	
+	public DefaultTableModel loadList(File file) throws FileNotFoundException
+	{
+		DefaultTableModel model = new DefaultTableModel(new Object[][] {
+				null,
+			},
+			new String[] {
+				"Name", "Size", "Value"
+			}
+		) {
+
+			private static final long serialVersionUID = 1L;
+
+			@SuppressWarnings("rawtypes")
+			Class[] columnTypes = new Class[] {
+				String.class, Integer.class, Integer.class
+			};
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		};
+		model.removeRow(0);
+		
+		Scanner in = new Scanner(file);
+		while(in.hasNext())
+		{
+			model.addRow(new Object[]{in.nextLine(),in.nextLine(),in.nextLine()});
+		}
+		in.close();
+		
+		return model;
+	}
+}
