a3c996468fcf783045418c2fc4f0238f5d779d42
Marcin Mikolajczyk <mikus12@gmail.com>
2013-05-20 13:43:56 +0200

    Another rework

--- src/GeneticAlgorithm/Strategies/Selection/NTournamentSelectionStrategy.java
+++ src/GeneticAlgorithm/Strategies/Selection/NTournamentSelectionStrategy.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package GeneticAlgorithm.Strategies.Selection;
 
@@ -16,7 +16,7 @@ import GeneticAlgorithm.Models.Population;
  *
  */
 public class NTournamentSelectionStrategy extends SelectionStrategy {
-    
+
     private final int tournamentSize;
 
     /**
@@ -29,35 +29,37 @@ public class NTournamentSelectionStrategy extends SelectionStrategy {
     @Override
     public List<Knapsack> select(Population population) {
         List<Knapsack> result = new ArrayList<>();
-        
-        while (result.size() < 2) {
-            Knapsack k = getBestFromTournament(population);
-            
-            if (!result.contains(k))
-                result.add(k);
-        }
-        
+
+        Knapsack k = getBestFromTournament(population, null);
+        result.add(k);
+
+        Knapsack l = getBestFromTournament(population, k);
+        result.add(l);
+
         assert(result.size() == 2);
-        
+
         return result;
     }
 
-    private Knapsack getBestFromTournament(Population population) {
+    private Knapsack getBestFromTournament(Population population, Knapsack knapsack) {
         List<Knapsack> tournament = new ArrayList<>();
-        
+
         while (tournament.size() < tournamentSize) {
             Knapsack k = population.getRandomKnapsack();
-            
+
+            if (knapsack != null && k == knapsack)
+                continue;
+
             if (!tournament.contains(k))
                 tournament.add(k);
         }
-        
+
         assert(tournament.size() == tournamentSize);
-        
+
         Collections.sort(tournament);
-        
+
         System.out.println("Wybrano plecak z fitness: " + tournament.get(0).fitness());
-        
+
         return tournament.get(0);
     }
 
